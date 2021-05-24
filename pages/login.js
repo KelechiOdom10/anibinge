182d5d3fc9f82f8374b94b339c5ab9d124036aef
kelechiodom10 <kelechiodom10@gmail.com>
2021-05-24 12:36:35 +0100

    Created custom login page

---
+++ pages/login.js
@@ -0,0 +1,30 @@
+import { Box } from "@chakra-ui/react";
+import LoginForm from "../components/LoginForm";
+import { getProviders, getSession, getCsrfToken } from "next-auth/client";
+
+export default function loginPage({ providers, csrfToken }) {
+  return (
+    <Box w="50%" mx="auto">
+      <LoginForm token={csrfToken} providers={providers} />
+    </Box>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { req, res } = context;
+  const session = await getSession({ req });
+  const csrfToken = await getCsrfToken(context);
+  const providers = await getProviders(context);
+
+  if (session && res && session.accessToken) {
+    res.writeHead(302, {
+      Location: "/",
+    });
+    res.end();
+    return;
+  }
+
+  return {
+    props: { providers, csrfToken },
+  };
+}
