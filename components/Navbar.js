7b0e71636d333f38a10cff4b0ebcd2406824a175
kelechiodom10 <kelechiodom10@gmail.com>
2021-05-24 12:32:11 +0100

    Added ability to sign in via github and passwordless auth

---
+++ components/Navbar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Flex, Heading, Link, Button, Avatar } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import NextLink from "next/link";
+import { signIn, signOut, useSession } from "next-auth/client";
+
+const MenuItem = ({ children, isLast, to = "/" }) => {
+  const router = useRouter();
+  const isActive = router.pathname === "/";
+
+  return (
+    <NextLink href={to} passHref>
+      <Link
+        mr={{ base: 0, sm: isLast ? 0 : 8 }}
+        fontSize={{ base: "sm", md: "md" }}
+        fontWeight="medium"
+        display="block"
+        color={isActive ? "red" : "blue"}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  );
+};
+
+export default function Navbar() {
+  const [session] = useSession();
+
+  return (
+    <Flex
+      justifyContent="space-between"
+      align="center"
+      bg="gray.50"
+      py={2}
+      px={4}
+    >
+      <Heading>Anibinge</Heading>
+
+      {!session ? (
+        <Button colorScheme="blue" onClick={signIn}>
+          Login / Sign up
+        </Button>
+      ) : (
+        <Flex align="center">
+          <Avatar
+            size="sm"
+            name={session.user.name ?? session.user.email}
+            color="white"
+            fontWeight="bold"
+            src={session.user.image}
+            mr={2}
+          />
+          <Button onClick={signOut}>Sign Out</Button>
+        </Flex>
+      )}
+    </Flex>
+  );
+}
