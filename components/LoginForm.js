2e5f6f311ede02a76f54446cc15ef7413967a827
kelechiodom10 <kelechiodom10@gmail.com>
2021-05-24 19:59:13 +0100

    Customised login component

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
   FormControl,
   FormLabel,
   Input,
+  InputGroup,
+  InputRightElement,
   Text,
   Link,
   Stack,
+  IconButton,
+  useToast,
 } from "@chakra-ui/react";
 import { signIn } from "next-auth/client";
-import { useRouter } from "next/router";
-import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiOutlineEyeInvisible,
+  AiOutlineEye,
+} from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
+import { useRouter } from "next/router";
+import FormDivider from "./FormDivider";
 
 const icons = {
   github: <AiFillGithub />,
@@ -21,10 +31,49 @@ const icons = {
 };
 
 export default function LoginForm({ providers, token }) {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = values;
+  const [show, setShow] = useState(false);
   const router = useRouter();
+  const toast = useToast();
+
+  const toggle = e => {
+    setShow(!show);
+  };
+
+  const handleChange = e => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    
+    if (result.error) {
+      toast({
+        title: "Error",
+        variant: "left-accent",
+        position: "top-right",
+        description: result.error,
+        status: "error",
+        isClosable: true,
+        duration: 4000,
+      });
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
     <Box>
-      <form method="post" action="/api/auth/signin/email">
+      <form onSubmit={handleSubmit}>
         <Text
           as="h2"
           fontSize="lg"
@@ -44,9 +93,33 @@ export default function LoginForm({ providers, token }) {
             <Input
               type="email"
               name="email"
+              value={email}
+              onChange={handleChange}
               placeholder="Enter your email address"
               fontSize={{ base: "sm", md: "md" }}
             />
+          </FormControl>
+          <FormControl id="password" isRequired>
+            <FormLabel>Password</FormLabel>
+            <InputGroup size="md">
+              <Input
+                pr="4.5rem"
+                type={show ? "text" : "password"}
+                name="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={handleChange}
+                fontSize={{ base: "sm", md: "md" }}
+              />
+              <InputRightElement width="3rem">
+                <IconButton
+                  h="1.75rem"
+                  onClick={toggle}
+                  colorScheme="gray"
+                  icon={show ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                />
+              </InputRightElement>
+            </InputGroup>
           </FormControl>
           <Button type="submit">Sign In</Button>
         </Stack>
@@ -59,8 +132,9 @@ export default function LoginForm({ providers, token }) {
           </Text>
         </Box>
       </form>
+      <FormDivider text="or" />
       {Object.values(providers).map(provider => {
-        if (provider.name === "Email") {
+        if (provider.name === "Credentials") {
           return;
         }
 
@@ -72,11 +146,10 @@ export default function LoginForm({ providers, token }) {
             my={2}
             leftIcon={icons[provider.id]}
             colorScheme={provider.id}
-            onClick={
-              () =>
-                signIn(provider.id, {
-                  callbackUrl: `${process.env.NEXTAUTH_URL}`,
-                }) // router.query.callbackUrl
+            onClick={() =>
+              signIn(provider.id, {
+                callbackUrl: `${process.env.NEXTAUTH_URL}`,
+              })
             }
           >
             Sign in with {provider.name}
